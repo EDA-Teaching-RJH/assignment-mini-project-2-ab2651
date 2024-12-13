cf8c167f6d720acd8d182faf658c41708ddf1a21
ab2651 <ab2651@kent.ac.uk>
2024-12-13 04:16:33 +0000

    ok

---
+++ file1.py
@@ -0,0 +1,25 @@
+class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def check_balance(self):
+        return self.balance
+    
+    def deposit_money(self, amount):
+        if amount <= 0:
+            print("Amount must be greater than 0.")
+            raise ValueError
+    
+    def withdraw_money(self, amount):
+        if amount <= 0:
+            print("Amount must be greater then 0.")
+            raise ValueError
+        if amount > self.balance:
+            print("Insufficient funds in the account.")
+            raise ValueError
+
+
+    
+
+
