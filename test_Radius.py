de66b33fac134098ec484d7aec656fd32f5e0a95
ab2651 <ab2651@kent.ac.uk>
2024-12-13 09:11:41 +0000

    ok

--- test_Radius.py
+++ test_Radius.py
@@ -1,2 +1,10 @@
+import pytest
+from Radius import Circle
 
-    
+def test_diameter():
+    radius = Circle(7)
+    assert radius.diameter() == 14
+
+def test_circumference():
+    radius = Circle(2.6)
+    assert radius.circumference() == 16.3362818
